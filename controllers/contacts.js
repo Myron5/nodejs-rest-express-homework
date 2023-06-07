356cc838ae97e5a7e3f24c15c6ac7be5f93304ca
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-06-07 21:40:22 +0300

    init

---
+++ controllers/contacts.js
@@ -0,0 +1,50 @@
+const contacts = require("../models/contacts.js");
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const getAll = async (_, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "contact deleted" });
+};
+
+const add = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  deleteById: ctrlWrapper(deleteById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+};
