5cd75723ad8c1c26244e51d5f3fb7f29b620e857
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-06-21 23:22:09 +0300

    fix middleware

--- middlewares/isUniqueEmail.js
+++ middlewares/isUniqueEmail.js
@@ -5,9 +5,9 @@ const isUniqueEmail = async (req, _, next) => {
   const { email } = req.body;
   const list = await listUsers();
   console.log(list);
-  const isInList = list.some(({ userEmail }) => userEmail === email);
+  const isInList = list.some((user) => user.email === email);
   if (isInList) {
-    next(HttpError(404, `${email} id already registered`));
+    next(HttpError(404, `${email} email already registered`));
   }
   next();
 };
